8687cf0baec4c1cb6a15cfa048e4cc4416d9f2a9
Your Name <shimbirosharon@gmail.com>
2017-02-09 18:43:19 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root 'static_pages#home'
+root'static_pages#home'
 
-  get 'help' 'static_pages/help'
-  get 'about' 'static_pages/about'
-  get  'contact' 'static_pages/contact'
-  get 'signup' 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'application#hello'
-end
+get 'help'=> 'static_pages#help'
+get 'about'=> 'static_pages#about'
+get 'contact' => 'static_pages#contact'
+get 'signup' => 'users#new'
+end 
